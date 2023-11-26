d3d545e73ef3f5aed079852f44847ca07b5cffab
UjasShah <89996494+UjasShah@users.noreply.github.com>
2023-11-25 20:08:20 -0700

    lab03 done

--- lab03/dags/etl_scripts/extract.py
+++ lab03/dags/etl_scripts/extract.py
@@ -16,17 +16,17 @@ def extract_data(date):
                      username= os.getenv('USERNAME'),
                      password = os.getenv('PASSWORD'))
     
-    # check if EXTRACT_DIR is empty
+    # check if there is an extracted folder in the bucket
     blobs = bucket.list_blobs(prefix=folder_name)
     if len(list(blobs)) == 0:
         #extract all data till the date
-        results = client.get("9t4d-g238", where = f"datetime <= '{date}'", limit = 2000) #CHANGE THIS NUMBER
+        results = client.get("9t4d-g238", where = f"datetime <= '{date}'", limit = 200000)
     else:
         #extract data for the date
         results = client.get("9t4d-g238", where = f"date_trunc_ymd(datetime) = '{date}'")
 
-    results_df = pd.DataFrame.from_records(results)
-
-    # upload to GCS
-    blob = bucket.blob(f"extracted/{date}_outcomes.csv")
-    blob.upload_from_string(results_df.to_csv(), 'text/csv')
+    if len(results) != 0:
+        results_df = pd.DataFrame.from_records(results)
+        # upload to GCS
+        blob = bucket.blob(f"extracted/{date}_outcomes.csv")
+        blob.upload_from_string(results_df.to_csv(index=False), 'text/csv')
